0b4cb0353d59576a214cd6891c77519c3ca1410a
wildenali <wildenwildenaliali@gmail.com>
2020-09-24 13:41:34 +0700

    Membuat 2 buah garis dan ditampilkan

---
+++ 02-Pendahulan_Pyplot/Main.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+Step-step membuat plot
+1. Membuat data
+2. Membuat plot
+3. Menampilkan plot
+"""
+
+# 1. Membuat data
+x = np.array([1, 2, 3, 4, 5])
+y = np.array([1, 4, 9, 16, 25])
+y2 = np.array([1, 16, 81, 256, 625])
+
+# 2. Membuat plot
+plt.plot(x, y)
+plt.plot(x, y2)
+
+# 3 Menampilkan plot
+plt.show()
