88996f85f30bb36909d5f9c2040824008ac4572a
wildenali <wildenwildenaliali@gmail.com>
2020-09-24 14:18:31 +0700

    Kasih warna dan marker pada plot

--- 03-SetWarna_dan_Marker/Main.py
+++ 03-SetWarna_dan_Marker/Main.py
@@ -24,5 +24,20 @@ print(t1)
 print(y1)
 plt.plot(t1, y1)
 
+# Ubah warna
+t2, y2 = sinusGenerator(1, 1, 4, 30)
+plt.plot(t2, y2, 'r')
+
+#  Pakai Marker
+t3, y3 = sinusGenerator(1, 1, 4, 60)
+plt.plot(t3, y3, 'b--')
+
+#  Pakai Marker
+t4, y4 = sinusGenerator(1, 1, 4, 90)
+plt.plot(t4, y4, 'c-o')
+
 # 3. Menampilkan plot
 plt.show()
+
+
+# Untuk marker sama warna yg tersedia cek di github atau link lainnya
